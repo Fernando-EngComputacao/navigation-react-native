f6d90629abaced8ce1679a4a5e5cd07d73504eda
Fernando Souza Furtado Carrilho <fernando.furtado@compasso.com.br>
2022-10-05 14:14:40 -0300

    timeStamp - useEffect

--- src/screens/Produtores/index.js
+++ src/screens/Produtores/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, Text, StyleSheet } from 'react-native';
 
 import Produtor from './components/Produtor';
 import Topo from './components/Topo';
 import useProdutores from '../../hooks/useProdutores';
 import useTextos from '../../hooks/useTextos';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 export default function Produtores({ melhoresProdutores }) {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const [exibirMensagem, setExibirMensagem] = useState(false);
 
   const lista = useProdutores(melhoresProdutores);
-  const { tituloProdutores } = useTextos();
+  const { tituloProdutores, mensagemCompra } = useTextos();
+
+  const nomeCompra = route.params?.compra.nome;
+  const timeStampCompra = route.params?.compra.timestamp;
+  const mensagemCompraCompleta = mensagemCompra?.replace('$NOME', nomeCompra);
+
+  useEffect(() => {
+    setExibirMensagem(!!nomeCompra);
+    let timeOut = (nomeCompra ? setTimeout(() => { setExibirMensagem(false)},3000) : null);
+  },[timeStampCompra]);
 
   const TopoLista = () => {
     return <>
       <Topo melhoresProdutores={melhoresProdutores} />
+      {exibirMensagem && <Text style={estilos.compraRelizada}> { mensagemCompraCompleta } </Text>}
       <Text style={estilos.titulo}>{tituloProdutores}</Text>
     </>
   }
@@ -45,5 +58,13 @@ const estilos = StyleSheet.create({
     marginTop: 16,
     fontWeight: 'bold',
     color: '#464646',
+  },
+  compraRelizada: {
+    backgroundColor: '#EAF5F3',
+    padding: 16,
+    color: '#464646',
+    fontSize: 15,
+    lineHeight: 25,
+    textAlign:'justify',
   }
 })
